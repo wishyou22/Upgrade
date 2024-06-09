a2bb0ab47a2d0c5a787ceb76754f610da098e31e
wishyou22 <koradavishal2209@gmail.com>
2024-06-09 15:38:51 +0530

    added all

--- upgrade/src/App/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../Features/Todo/TodoSlice';
-
-export const store = configureStore({
-  reducer: {
-    todos: todoReducer
-  }
-});
